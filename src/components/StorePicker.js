11a359b385d3c86da5607170ff7ca8378fbcec97
Admin <admin@Sophias-MacBook-Pro-2.local>
2018-11-07 00:02:53 +0100

    Added CSS styling and Store Picker component

---
+++ src/components/StorePicker.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+	render() {
+		return (
+			<form className="store-selector">
+				{ /* Hello */ }
+				<h2>Please Enter A Store</h2>
+				<input type="text" required placeholder="Store Name" />
+				<button type="submit">Visit Store →</button>
+			</form> 
+			)
+	}
+}
+
+export default StorePicker;
