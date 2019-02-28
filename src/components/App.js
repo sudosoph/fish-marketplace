420e49ffc930d5a8cdf192ee7821c701434e59e8
Sophia <sophiaraji@gmail.com>
2019-02-28 23:23:51 +0100

    Add sample fish data

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
 	constructor() {
@@ -9,6 +10,7 @@ class App extends React.Component {
 		super();
 
 		this.addFish = this.addFish.bind(this);
+		this.loadSamples = this.loadSamples.bind(this);
 
 		this.state = {
 			fishes: {},
@@ -28,6 +30,12 @@ class App extends React.Component {
 
 	}
 
+	loadSamples() {
+		this.setState({
+			fishes: sampleFishes
+		});
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -35,7 +43,7 @@ class App extends React.Component {
 					<Header tagline="Fish Marketplace" />
 				</div>
 				<Order />
-				<Inventory addFish = {this.addFish} />
+				<Inventory addFish = {this.addFish} loadSamples = {this.loadSamples} />
 			</div>
 		)
 	}
