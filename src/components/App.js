d62bd6c994939e749f75f330d2851467e2556609
Sophia <sophiaraji@gmail.com>
2019-03-01 00:43:45 +0100

    Update order state by mapping fish index to AddToOrder() function on click

--- src/components/App.js
+++ src/components/App.js
@@ -12,6 +12,7 @@ class App extends React.Component {
 
 		this.addFish = this.addFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
+		this.addToOrder = this.addToOrder.bind(this);
 
 		this.state = {
 			fishes: {},
@@ -37,6 +38,13 @@ class App extends React.Component {
 		});
 	}
 
+	addToOrder(key) {
+		//take a copy of our state
+		const order = {...this.state.order};
+		order[key] = order[key] + 1 || 1;
+		this.setState({ order });
+	}
+
 	render() {
 		return (
 			<div className="catch-of-the-day">
@@ -46,7 +54,7 @@ class App extends React.Component {
 						{
 							Object
 								.keys(this.state.fishes)
-								.map(key => <Fish key={key} details ={this.state.fishes[key]} />)
+								.map(key => <Fish key={key} index={key} details={this.state.fishes[key]} addToOrder={this.addToOrder} />)
 						}
 					</ul>
 				</div>
