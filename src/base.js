bba5c6eca1c90f11b23bf9d9b9b628dfa86b6e57
Sophia <sophiaraji@gmail.com>
2019-03-02 15:48:05 +0100

    Add Firebase to sync stores and fishes

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyBlVRO399MHvPLor9fr2S0Jdu_GeNHJx3w",
+    authDomain: "fish-marketplace.firebaseapp.com",
+    databaseURL: "https://fish-marketplace.firebaseio.com"
+});
+
+export default base;
