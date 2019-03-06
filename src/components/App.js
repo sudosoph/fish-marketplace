0f9e590adce13c3767de66bbdfd2739a6b246283
Sophia <sophiaraji@gmail.com>
2019-03-06 01:38:58 +0100

    Remove order from state

--- src/components/App.js
+++ src/components/App.js
@@ -13,8 +13,10 @@ class App extends React.Component {
 
 		this.addFish = this.addFish.bind(this);
 		this.updateFish = this.updateFish.bind(this);
+		this.removeFish = this.removeFish.bind(this);
 		this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
+		this.removeFromOrder = this.removeFromOrder.bind(this);
 
 		this.state = {
 			fishes: {},
@@ -69,6 +71,12 @@ class App extends React.Component {
 		this.setState({fishes});
 	}
 
+	removeFish(key) {
+		const fishes = {...this.state.fishes};
+		fishes[key] = null;
+		this.setState({fishes}); 
+	}
+
 	loadSamples() {
 		this.setState({
 			fishes: sampleFishes
@@ -79,6 +87,13 @@ class App extends React.Component {
 		//take a copy of our state
 		const order = {...this.state.order};
 		order[key] = order[key] + 1 || 1;
+		this.setState({order});
+	}
+
+	removeFromOrder(key) {
+		//take a copy of our state
+		const order = {...this.state.order};
+		delete order[key];
 		this.setState({order});
 	}
 
@@ -99,12 +114,14 @@ class App extends React.Component {
 					fishes={this.state.fishes} 
 					order={this.state.order} 
 					params={this.props.params}
+					removeFromOrder={this.removeFromOrder}
 				/>
 				<Inventory 
 					addFish={this.addFish} 
 					loadSamples={this.loadSamples} 
 					fishes={this.state.fishes}
 					updateFish={this.updateFish}
+					removeFish={this.removeFish}
 				/>
 			</div>
 		)
--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -35,6 +35,7 @@ class Inventory extends React.Component {
 				onChange={(e) => this.handleChange(e, key)}/>
 				<input type="text" name="image" value={fish.image} placeholder="Fish Image" 
 				onChange={(e) => this.handleChange(e, key)}/>
+				<button onClick={() => this.props.removeFish(key)}>Remove Fish</button>
 			</div>
 		)
 	}
